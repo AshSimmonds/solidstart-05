edcd5785f313e3505a5bbdac69bdacbbb611fece
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-15 11:50:18 +1030

    add gruntify router to trpc

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,7 +1,8 @@
 import { t } from "../utils";
 import exampleRouter from "./example";
 import notesRouter from "./notes";
+import gruntifyRouter from "./gruntify"
 
-export const appRouter = t.mergeRouters(exampleRouter, notesRouter);
+export const appRouter = t.mergeRouters(exampleRouter, notesRouter, gruntifyRouter)
 
 export type IAppRouter = typeof appRouter;
