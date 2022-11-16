472376ec08bcaf067aea31f50bcc0a39d7c0a590
Ash Simmonds <ashsimmonds@gmail.com>
2022-11-16 11:35:15 +1030

    VIGILO CONFIDO

---
+++ src/routes/xcom.tsx
@@ -0,0 +1,20 @@
+import PageTitle from "~/components/PageTitle";
+
+export default function XComCentralPage() {
+    return (
+        <>
+            <PageTitle>Central</PageTitle>
+            <h1>Central</h1>
+
+            <div class="text-center mx-auto ">
+
+                <h2>Central</h2>
+
+                <div>
+                    XState POC: https://codesandbox.io/s/xstate-solid-example-dgpd7?file=/index.jsx
+                </div>
+
+            </div>
+        </>
+    )
+}
